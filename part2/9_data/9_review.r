29513e8c59527d8cb2fb4fbef83a5f71dc28abb8
jun931223 <jun931223@gmail.com>
2022-05-01 22:14:33 +0900

    study: how to import Data and edit data

---
+++ part2/9_data/9_review.r
@@ -0,0 +1,38 @@
+# 벡터는 하나의 타입에 대해서만 저장할 수 있다.
+vector.test <- c(1:5)
+vector.test + 2
+
+# 리스트는 모든 타입을 저장할 수 있다.
+list.test <- list(num = 1, name = "name", age = 20)
+list.test[1]
+list.test$num
+
+# 리스트에 벡터 포함가능
+list.test2 <- list(vect = vector.test, name = "벡터")
+list.test2$vect
+
+# 팩터는 정해진 범위 안의 문자를 활용하는 경우에 사용된다.
+  # 문자형 백터를 만든 후 factor() 함수를 이용해서 팩터로 치환
+subject <- c("과학","수학","물리")
+factor.test <- factor(subject)
+factor.test
+
+# 매트릭스 개념과 생성
+matrix.test <- matrix(1:10, nrow=5, ncol=2, byrow=T)
+matrix.test
+
+# 백터를 열방향으로 묶어서 매트릭스 생성
+i <- c(1:5)
+j <- c(6:10)
+cbind(i,j)
+rbind(i,j)
+
+# 데이터 프레임
+# 2차원인 테이블의 형태로 데이터를 표현하며 숫자, 문자 등 서로 다른 형태 데이터를 표현 가능
+name <- c("김한양","이한양","너한양")
+score <- c(100,99,98)
+report_bind <- cbind(name,score)
+report_bind
+
+report_info <- data.frame(name, score)
+report_info
---
+++ part2/9_data/importData.r
@@ -0,0 +1,18 @@
+# write.table 일반 텍스트 파일 쓰기
+# read.table 일반 텍스트 파일 읽기
+# write.csv csv 파일 쓰기
+# read.csv csv 파일 읽기
+test_1 <- read.table("data/data.txt", header = T)
+str(test_1)
+
+write.table(test_1, file = "data/dataWithoutQuote.txt", quote = F)
+write.table(test_1, file = "data/dataWithQuote.txt")
+
+test_2 <- read.table("data/dataWithDot.txt", header = T, sep = ",", as.is= T)
+str(test_2)
+
+test_3 <- read.csv("data/Book1.csv")
+str(test_3)
+
+test_4 <- read.csv("data/Book1.csv", col.names = c("Name","C","JAVA","PYTHON"),row.names = c("김한양","이한양","정한양","리한양","황한향"))
+write.csv(test_4, file = "data/addRowAndColNames.csv" , quote = F )
